b8fb5fda854b10f8ec0f4e1a59b7026485ebc0c1
shamilbin <shamiltk029@gmail.com>
2025-11-21 05:11:56 +0530

    controllers

---
+++ server/controllers/workspaceController.js
@@ -0,0 +1,81 @@
+//  get all work space for user
+
+import { memo } from "react";
+import prisma from "../configs/prisma.js";
+
+export const getUserWorkspaces = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const workspaces = await prisma.workspace.findMany({
+      where: {
+        members: { some: { userId: userId } },
+      },
+      include: {
+        members: { include: { user: true } },
+        projects: {
+          include: {
+            tasks: {
+              include: {
+                assignee: true,
+                comments: { include: { user: true } },
+              },
+            },
+            members: {
+              include: { user: true },
+            },
+          },
+        },
+        owner: true,
+      },
+    });
+    res.json({ workspaces });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
+
+//  add member fuction to work space
+
+export const addMember = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const {email,role ,workspaceId,message}=req.body
+
+    //  check if user exisit
+
+    const user = await prisma.user.findUnique({where:{email}})
+
+    if(!user){
+        return res.status(404).json({message:"User not found"})
+    }
+    if(!workspaceId || !role){
+         return res.status(400).json({message:"Missing Required Parameters"})
+    }
+     if(![`ADMIN`,`MEMBER`].includes(role)){
+         return res.status(400).json({message:"Invalid Role"})
+    }
+    //  fetch workspace
+    const workspace = await prisma.workspace.findUnique({where:{id:workspaceId},include:{members:true}})
+
+    if(!workspace){
+          return res.status(404).json({message:"workspace not found"})
+
+    }
+    //  check creator has admin role
+
+    if(!workspace.members.find((member)=>member.userId===userId && member.role===`ADMIN`)){
+         return res.status(401).json({message:"You Dont Have ADMIN Privilages"})
+    }
+    //  check if user is already a member
+    const existingMember = workspace.members.find((member)=>member.userId===userId)
+    if(existingMember){
+          return res.status(400).json({message:"User is already a member"})
+
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
