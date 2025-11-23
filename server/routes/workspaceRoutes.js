3ac4b555f84bc57f632ece9dddaa8564a00b9abf
shamilbin <shamiltk029@gmail.com>
2025-11-23 23:17:13 +0530

    routes for workspace

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -4,4 +4,5 @@ import { getUserWorkspaces } from "../controllers/workspaceController.js"
 const workspaceRouter=express.Router()
 
 workspaceRouter.get("/",getUserWorkspaces)
+
 workspaceRouter.post("/add-member",getUserWorkspaces)
